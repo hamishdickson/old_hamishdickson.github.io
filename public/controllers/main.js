bde2b92e01076a57b3fae7dba640676e79dacaae
mishyTheBear <hamish.dickson@gmail.com>
2014-12-11 16:03:44 +0000

    Start creating a landing page

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -6,5 +6,12 @@
 
     app.controller('MainController', function () {
 
+        $(function () {
+            $('#comet').animate({left: '+=100px', top: '+=100px'}, 1000, 'linear');
+        });
+
+        $(function () {
+            $('#comet').animate({left: '+=250px', top: '+=100'}, 1000, 'linear');
+        });
     });
 })();
