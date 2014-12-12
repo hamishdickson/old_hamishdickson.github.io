e779b27b14e4c951722f0e6452809c7439b3cdd6
mishyTheBear <hamish.dickson@gmail.com>
2014-12-12 15:43:48 +0000

    add nextStep function

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -15,29 +15,37 @@
         var MARS_X = 1000,
             MARS_Y = 200;
 
-        var starting_point_x = 0,
-            starting_point_y = 0;
+        var STARTING_POINT_X = 0,
+            STARTING_POINT_Y = 0;
+
+        var STARTING_VELOCITY_X = 0,
+            STARTING_VELOCITY_Y = 0;
 
         var G = 1;
 
-        var INITIAL_VELOCITY_X = 0,
-            INITIAL_VELOCITY_Y = 0;
+        var INITIAL_VELOCITY_X = 100,
+            INITIAL_VELOCITY_Y = 100;
 
-        var TIME_STEP = 1000; // miniseconds
+        var TIME_STEP = 10; // miniseconds
 
-        var x = starting_point_x,
-            y = starting_point_y;
+        var x = STARTING_POINT_X,
+            y = STARTING_POINT_Y;
 
-        var delta_x = 0,
-            delta_y = 0;
-        
+        var u_x = STARTING_VELOCITY_X,
+            u_y = STARTING_VELOCITY_Y;
+
+        var delta_x = 100,
+            delta_y = 100;
+
+        this.setNextStep = function() {
+            // s = ut + 0.5at^2
+
+            delta_x = u_x * TIME_STEP;
+            delta_y = u_y * TIME_STEP;
+        };
 
         $(function () {
-            $('#comet').animate({left: '+=100px', top: '+=100px'}, 1000, 'linear');
-        });
-
-        $(function () {
-            $('#comet').animate({left: '+=250px', top: '+=100'}, 1000, 'linear');
+            $('#comet').animate({left: '+=' + delta_x + "px", top: '+=' + delta_y + "px"}, 1000, 'linear');
         });
     });
 })();
