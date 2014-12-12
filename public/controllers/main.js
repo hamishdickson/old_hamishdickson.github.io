0e9a81ddd76d2cbf4725b8856e3552ce6bd64da5
mishyTheBear <hamish.dickson@gmail.com>
2014-12-12 15:08:46 +0000

    Start implementing gravity

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -6,6 +6,33 @@
 
     app.controller('MainController', function () {
 
+        var sun_x = 200,
+            sun_y = 200;
+
+        var earth_x = 500,
+            earth_y = 500;
+
+        var mars_x = 200,
+            mars_y = 700;
+
+        var starting_point_x = 0,
+            starting_point_y = 0;
+
+        var G = 1;
+
+        var initial_velocity_x = 0,
+            initial_velocity_y = 0;
+
+        var time_step = 1000; // miniseconds
+
+        var x = starting_point_x,
+            y = starting_point_y;
+
+        var delta_x = 0,
+            delta_y = 0;
+
+        
+
         $(function () {
             $('#comet').animate({left: '+=100px', top: '+=100px'}, 1000, 'linear');
         });
