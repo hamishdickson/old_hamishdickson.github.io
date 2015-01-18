7bf740228e2e23aaae4729451134ae128e0d3131
mishyTheBear <hamish.dickson@gmail.com>
2015-01-18 14:52:06 +0000

    add gol

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -6,7 +6,6 @@
 
     app.controller('MainController', function () {
 
-        // todo when happy remove the jQuery files et
 /*
         var SUN_X = 200,
             SUN_Y = 200;
@@ -185,5 +184,90 @@
         function getRandomArbitrary(min, max) {
             return Math.random() * (max - min) + min;
         }
+
+        // ----------------- game of life --------------------
+
+        var HEIGHT = 20;
+        var WIDTH = 20;
+
+        var grid = [
+            [true, true, true, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false],
+            [false, false, false, false, false]
+        ];
+
+
+        var isCellAlive = function(x, y) {
+            return isCellOnGrid(x, y) && grid[x][y];
+        };
+
+        var iterate = function() {
+            var newGrid;
+
+            for (j = 0; j < HEIGHT; j++) {
+                for (i = 0; i < WIDTH; i++) {
+                    var numberOfNeighbours = countNeighbours(i, j);
+                    if (numberOfNeighbours == 2 || numberOfNeighbours == 3) {
+                        newGrid[i][j] = true;
+                    } else {
+                        newGrid[i][j] = false;
+                    }
+                }
+            }
+
+            grid = newGrid;
+        };
+
+        var isCellOnGrid = function(x, y) {
+            return (x >= 0 && y >= 0 && x <= WIDTH - 1 && y <= HEIGHT - 1);
+        };
+
+        var countNeighbours = function(x, y) {
+            var count = 0;
+            if (isTopLeftCellAlive(x, y)) count++;
+            if (isTopCellAlive(x, y)) count++;
+            if (isTopRightCellAlive(x, y)) count++;
+            if (isLeftCellAlive(x, y)) count++;
+            if (isRightCellAlive(x, y)) count++;
+            if (isBottomLeftCellAlive(x, y)) count++;
+            if (isBottomCellAlive(x, y)) count++;
+            if (isBottomRightCellAlive(x, y)) count++;
+            return count;
+        };
+
+        var isBottomRightCellAlive = function(x, y) {
+            return isCellAlive(x + 1, y + 1);
+        };
+
+        var isBottomCellAlive = function(x, y) {
+            return isCellAlive(x, y + 1);
+        };
+
+        var isBottomLeftCellAlive = function (x, y) {
+            return isCellAlive(x - 1, y + 1);
+        };
+
+        var isRightCellAlive = function(x, y) {
+            return isCellAlive(x + 1, y);
+        };
+
+        var isLeftCellAlive = function(x, y) {
+            return isCellAlive(x - 1, y);
+        };
+
+        var isTopRightCellAlive = function(x, y) {
+            return isCellAlive(x + 1, y - 1);
+        };
+
+        var isTopCellAlive = function(x, y) {
+            return isCellAlive(x, y - 1);
+        };
+
+        var isTopLeftCellAlive = function(x, y) {
+            return isCellAlive(x-1, y-1);
+        };
+
     });
 })();
