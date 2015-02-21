338749dbc3669985f75669e081f4205406825547
mishyTheBear <hamish.dickson@gmail.com>
2015-02-21 14:59:06 +0000

    public

--- public/controllers/instagram.js
+++ public/controllers/instagram.js
@@ -10,7 +10,8 @@
             restrict: 'E',
             templateUrl: 'public/views/instagram.html',
             controller: function () {
-                var instagramApiUrl = "https://api.instagram.com/v1/users/self/feed?client_id=1e35c120cf174b948e06c09bb6f5422a";
+                //var instagramApiUrl = "https://api.instagram.com/v1/users/self/feed?client_id=1e35c120cf174b948e06c09bb6f5422a";
+                var instagramApiUrl = "https://api.instagram.com/v1/media/popular?client_id=1e35c120cf174b948e06c09bb6f5422a";
 
                 var pics = [];
 
