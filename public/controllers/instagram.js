6f30274e38b2360606de5e0f9a47d819a2460f3e
mishyTheBear <hamish.dickson@gmail.com>
2015-02-21 13:18:30 +0000

    ooo

--- public/controllers/instagram.js
+++ public/controllers/instagram.js
@@ -10,7 +10,7 @@
             restrict: 'E',
             templateUrl: 'public/views/instagram.html',
             controller: function () {
-                var instagramApiUrl = "https://api.instagram.com/v1/users/self/feed?client_id=1e35c120cf174b948e06c09bb6f5422a&callback=callbackFunction";
+                var instagramApiUrl = "https://api.instagram.com/v1/users/self/feed?client_id=1e35c120cf174b948e06c09bb6f5422a";
 
                 var pics = [];
 
