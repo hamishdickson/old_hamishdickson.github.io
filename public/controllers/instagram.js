a3ad2b5d940b1c10d53a160d391f65ecd12a1165
mishyTheBear <hamish.dickson@gmail.com>
2015-03-01 11:13:01 +0000

    ehy?

--- public/controllers/instagram.js
+++ public/controllers/instagram.js
@@ -11,6 +11,7 @@
             templateUrl: 'public/views/instagram.html',
             controller: function () {
                 //var instagramApiUrl = "https://api.instagram.com/v1/media/popular?client_id=1e35c120cf174b948e06c09bb6f5422a";
+                var instagramApiUrl = "https://api.instagram.com/v1/users/self/feed?access_token=27979401.1e35c12.80379493f0604b47a60ee2873cac3c13";
 
                 var pics = [];
 
