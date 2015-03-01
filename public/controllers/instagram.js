80e96e71bc95b9a457097f77259646010abf9434
mishyTheBear <hamish.dickson@gmail.com>
2015-03-01 11:19:21 +0000

    use jsonp

--- public/controllers/instagram.js
+++ public/controllers/instagram.js
@@ -13,12 +13,11 @@
                 var bob = "token=27979401.1e35c12.80379493f0604b47a60ee2873cac3c13";
                 var u = "instagram.com";
 
-                //var instagramApiUrl = "https://api.instagram.com/v1/media/popular?client_id=1e35c120cf174b948e06c09bb6f5422a";
                 var instagramApiUrl = "https://api." + u + "/v1/users/self/feed?access_" + bob;
 
                 var pics = [];
 
-                $http.get(instagramApiUrl)
+                $http.jsonp(instagramApiUrl)
                     .success(function(response) {
                         pics.push(response.data);
                     })
